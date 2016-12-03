740e36b8ffe619255242da2c45911da2c708ed2a
Matt White <mattwhite1337@gmail.com>
2016-12-03 17:57:52 -0500

    Refresh will get you a new word, will update with correct keys

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var arrOfWords = ["cat", "horse", "rabbit"];
+var arrOfWords = ["cat", "duck", "horse"];
 
 
 function placeHolder(string) {
@@ -9,24 +9,44 @@ function placeHolder(string) {
 	return arr
 }
 
+//Remember, arr is modified and Stays that way
 function replace(arr, string, key) {
 	if (string.indexOf(key) !== -1) {
 		arr[string.indexOf(key)] = key;
-		console.log(arr);
+		return arr
 	}
 }
 
+//Choose a random word from arrOfWords 
+var guess = arrOfWords[Math.floor(Math.random() * arrOfWords.length)];
+console.log("The var guess = " + guess);
 
 //This is where our guesses will show up
 var answerDiv = document.getElementById("answer");
 
-//Place the placeholder for a random word from the arrOfWords array
-answerDiv.innerHTML = placeHolder(arrOfWords[Math.floor(Math.random() * arrOfWords.length)]).join(" ");
+//Place the placeholder for the random word from the arrOfWords array
+answerDiv.innerHTML = placeHolder(guess).join(" ");
+
+//Save our placeHolder array for later
+var array = placeHolder(guess);
+console.log("The var array = " + array);
 
 
 document.onkeyup = function(event) {
 
 	keystroke = event.key;
 
+	//toReplace is replaced with the modifed array from replace();
+	if (guess.indexOf(keystroke) !== -1) {
+		var toReplace = replace(array, guess, keystroke);
+		answerDiv.innerHTML = toReplace.join(" ");
+	} else {
+		alert("That's not the right letter!");
+	}
 
 };
+
+
+
+
+
